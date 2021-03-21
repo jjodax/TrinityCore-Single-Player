96c973b515d2e87419b3ca9b7d32d3fa9f769d91
zaicopx <zaicopx@yahoo.com>
2021-03-21 13:08:35 +0700

    Autobalance

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -32,6 +32,8 @@ class bot_ai;
 class bot_pet_ai;
 //end npcbot
 
+#include "CustomData.h"
+
 class CreatureAI;
 class CreatureGroup;
 class Group;
@@ -329,6 +331,8 @@ class TC_GAME_API Creature : public Unit, public GridObject<Creature>, public Ma
         void LowerPlayerDamageReq(uint32 unDamage);
         void ResetPlayerDamageReq() { m_PlayerDamageReq = GetHealth() / 2; }
         uint32 m_PlayerDamageReq;
+
+        CreatureCustomData CustomData;
 
         uint32 GetOriginalEntry() const { return m_originalEntry; }
         void SetOriginalEntry(uint32 entry) { m_originalEntry = entry; }
